bc2d37555a26c87391a6131f03b3f4f4fe168bf4
AnandhaKaarthick <ansreesha1847@gmail.com>
2025-11-29 12:55:47 +0530

    new

---
+++ learn C programming/function/funWithoutArgAndRetu.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void greet();
+
+int main(){
+    printf("starting the program....\n");
+    greet();
+    printf("program is ended\n");
+    return 0;
+}
+
+void greet(){
+    printf("Hello....\n");
+    printf("Welcome to C programming.........\n");
+}
