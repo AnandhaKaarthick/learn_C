4de85474c166a5ab1353d5089a7844d31d06bcd4
AnandhaKaarthick <ansreesha1847@gmail.com>
2026-01-04 11:21:50 +0530

    new

---
+++ learn C programming/structure/StudentRecordSystem.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+typedef struct student{
+    char name[50];
+    char branch[4];
+    int rollno;
+    int marks;
+    int avg;
+}stu;
+
+int main(){
+    stu;
+    int n;
+    printf("Enter the number of students: ");
+    scanf("%d", n);
+
+    for(i = 0; i<n; i++){
+        printf("Enter the student%d name: ");
+        scanf()
+    }
+}
